04edab78b1cca2465c84430b49ca4d477ecc8a6d
Zubair2k <96760145+Zubair2k@users.noreply.github.com>
2022-04-07 10:10:01 +0530

    Add files via upload

---
+++ Task(TO-DO_LIST)/todoform.js
@@ -0,0 +1,39 @@
+let addtodo=document.getElementById('addtodo');
+let todoContainer=document.getElementById('todoContainer');
+let inputText=document.getElementById('inputtext');
+let clrall=document.getElementById('clrall');
+
+addtodo.addEventListener('click',function(){
+    var para=document.createElement('p');
+    para.innerText=inputText.value;
+    todoContainer.appendChild(para);
+    inputText.value="";
+    para.addEventListener('click',function(){
+        para.style.textDecoration="line-through";
+        clr.addEventListener('click',function(){
+            todoContainer.removeChild(para);
+        })
+    })
+    
+    para.addEventListener('dblclick',function(){
+        todoContainer.removeChild(para);
+    })
+
+})
+
+clrall.addEventListener('click',function(){
+    todoContainer.innerHTML="";
+})
+
+save.addEventListener('click',function(){
+    var savebtn=document.getElementById('todoContainer').innerHTML;
+    localStorage.setItem('savebtn',savebtn);
+
+})
+
+savelst.addEventListener('click',function(){
+    var savelst=localStorage.getItem('savebtn');;
+    document.getElementById('todoContainer').innerHTML= savelst;
+
+})
+
